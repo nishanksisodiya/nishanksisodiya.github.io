17deef8d49d214b69951a03fc7e524be13d3a50e
Nishank Sisodiya <nishanksisodiya@gmail.com>
2024-09-28 22:21:40 +0530

    Added acc

---
+++ _next/static/el6xOJAjowvhYvUGRjFPm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/75fc9c18-677b9b304ed99070.js","static/chunks/618-cd94d2436646c5ea.js","static/chunks/pages/index-e5faa5b347a61b94.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
